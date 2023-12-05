22a59a1e11c412dc8459db397f2b0816f04c5d4c
Vishnu Kulkarni <vishnu@starapps.studio>
2023-12-06 00:27:26 +0530

    minor changes  to main route and upgrade  nextjs to 14

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
+import BottomNav from '@/components/common/BottomNav'
+import LeftSidebar from '@/components/common/LSideBar'
+import PreviewScreen from '@/components/custom-ui/personalize/PreviewScreen'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+
     </main>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,46 +1,42 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      serverActions: true,
-    },
-      images: {
-          remotePatterns: [
-            {
-              protocol: "https",
-              hostname: "img.clerk.com",
-            },
-            {
-              protocol: "https",
-              hostname:"wp-mktg.prod.getty1.net",
-             
-            },
-            {
-              protocol: "https",
-              hostname: "images.clerk.dev",
-            },
-            {
-              protocol: "https",
-              hostname: "uploadthing.com",
-            },
-            {
-              protocol: "https",
-              hostname: "placehold.co",
-            },
-            {
-              protocol: "https",
-              hostname: "unsplash.com",
-            }, {
-              protocol: "https",
-              hostname: "cdn.pixabay.com",
-            },
-            {
-              protocol: "https",
-              hostname: "utfs.io",
-            }
-          
-          ],
-        },
-  }
-  
-  module.exports = nextConfig
-  
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "wp-mktg.prod.getty1.net",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
